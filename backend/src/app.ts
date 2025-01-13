ae2b8f3ad09a9843ad3e55f69f322cd8447bc09a
Himanshu Kumar <himankshu1@gmail.com>
2025-01-13 20:23:57 +0530

    add global error handler in express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,28 @@
-import express, { Request, Response } from "express";
+import logger from "./config/logger";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
     res.send("hello");
+});
+
+//? Global Error Handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
