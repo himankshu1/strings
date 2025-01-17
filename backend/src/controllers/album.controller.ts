102688f076783f3bdbb63178baa0b75615dfec84
Himanshu Kumar <himankshu1@gmail.com>
2025-01-17 14:17:27 +0530

    get album by id controller

--- backend/src/controllers/album.controller.ts
+++ backend/src/controllers/album.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { Album } from '../models/album.model';
 import logger from '../config/logger';
 
+//* get all albums
 export const getAllAlbums = async (req: Request, res: Response) => {
     try {
         //* query all album
@@ -15,6 +16,49 @@ export const getAllAlbums = async (req: Request, res: Response) => {
             success: true,
             message: 'all albums retrieved',
             data: albums,
+        });
+    } catch (err) {
+        if (err instanceof Error) {
+            logger.error('something went wrong while fetching all albums', {
+                error: err.message,
+            });
+        } else {
+            logger.error('something went wrong while fetching all albums', {
+                error: JSON.stringify(err),
+            });
+        }
+    }
+};
+
+//* get an album by id
+export const getAlbumById = async (req: Request, res: Response) => {
+    try {
+        const { albumId } = req.params;
+
+        //* if album id not provided
+        if (!albumId) {
+            res.status(400).json({
+                success: false,
+                message: 'Invalid request! Please provide the album id',
+            });
+        }
+
+        //* finding the album document
+        const album = await Album.findById(albumId);
+
+        //* if album not found by the id
+        if (!album) {
+            res.status(404).json({
+                success: false,
+                message: 'Incorrect album id or album not available',
+            });
+        }
+
+        //* success response
+        res.status(200).json({
+            success: true,
+            message: 'album found!',
+            data: album,
         });
     } catch (err) {
         if (err instanceof Error) {
--- backend/src/routes/album.route.ts
+++ backend/src/routes/album.route.ts
@@ -1,8 +1,9 @@
-import { getAllAlbums } from '../controllers/album.controller';
+import { getAlbumById, getAllAlbums } from '../controllers/album.controller';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/get-albums', getAllAlbums);
+router.get('/get-album/:albumId', getAlbumById);
 
 export default router;
