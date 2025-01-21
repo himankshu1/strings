ba3bdfa372aeadc65af4ed9c5eb65a76368a310d
Himanshu Kumar <himankshu1@gmail.com>
2025-01-21 19:38:28 +0530

    add react router and project init

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import AuthCallbackPage from './pages/AuthCallbackPage';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/auth-callback" element={<AuthCallbackPage />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+);
