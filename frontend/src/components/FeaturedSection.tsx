54aea7b7961d1231373e4a2744fad506b02629b5
Himanshu Kumar <himankshu1@gmail.com>
2025-01-30 12:56:00 +0530

    add featured section

---
+++ frontend/src/components/FeaturedSection.tsx
@@ -0,0 +1,36 @@
+import { MusicContext } from '@/providers/MusicProvider';
+import { useContext } from 'react';
+import FeaturedGridSkeleton from './skeletons/FeaturedGridSkeleton';
+
+const FeaturedSection = () => {
+    const { featuredSongs, isLoading } = useContext(MusicContext)!;
+
+    if (isLoading) return <FeaturedGridSkeleton />;
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+            {featuredSongs.map((song) => (
+                <div
+                    key={song._id}
+                    className="flex items-center bg-zinc-800/50 rounded-md overflow-hidden
+         hover:bg-zinc-700/50 transition-colors group cursor-pointer relative"
+                >
+                    <img
+                        src={song.imageUrl}
+                        alt={song.title}
+                        className="w-16 sm:w-20 h-16 sm:h-20 object-cover flex-shrink-0"
+                    />
+                    <div className="flex-1 p-4">
+                        <p className="font-medium truncate">{song.title}</p>
+                        <p className="text-sm text-zinc-400 truncate">
+                            {song.artist}
+                        </p>
+                    </div>
+                    {/* <PlayButton song={song} /> */}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default FeaturedSection;
