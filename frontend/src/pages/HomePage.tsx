cad9ea649df6b0e1476ffb767f1cefd272f618d7
Himanshu Kumar <himankshu1@gmail.com>
2025-01-30 13:00:21 +0530

    add featured section to homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
+import FeaturedSection from '@/components/FeaturedSection';
 import Topbar from '@/components/Topbar';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { MusicContext } from '@/providers/MusicProvider';
 import { useContext } from 'react';
 
@@ -6,11 +8,26 @@ export default function HomePage() {
     const { featuredSongs, madeForYouSongs, trendingSongs } =
         useContext(MusicContext)!;
 
-    console.log(featuredSongs, madeForYouSongs, trendingSongs);
+    console.log(featuredSongs);
+    console.log(madeForYouSongs);
+    console.log(trendingSongs);
 
     return (
-        <div>
+        <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
             <Topbar />
-        </div>
+            <ScrollArea className="h-[calc(100vh-180px)]">
+                <div className="p-4 sm:p-6">
+                    <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+                        Good Afternoon
+                    </h1>
+                    <FeaturedSection />
+                </div>
+
+                <div className="space-y-8">
+                    <p>made for you</p>
+                    <p>trending</p>
+                </div>
+            </ScrollArea>
+        </main>
     );
 }
